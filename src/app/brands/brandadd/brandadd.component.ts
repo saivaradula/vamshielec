c90d66e01d1da5df4e230f09747df6d9039c43dc
Sai Varadula <svaradula@launchcg.com>
2020-08-08 14:04:33 +0530

    adding form-data for brands

--- src/app/brands/brandadd/brandadd.component.ts
+++ src/app/brands/brandadd/brandadd.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from "@angular/core";
 import { BrandsService } from "src/app/services/brands.service";
 import { Router } from "@angular/router";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+
 @Component({
   selector: "app-brandadd",
   templateUrl: "./brandadd.component.html",
@@ -9,12 +16,21 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 })
 export class BrandaddComponent implements OnInit {
   brandsDetails: any;
+  brandForm: FormGroup;
 
   constructor(
     private BS: BrandsService,
     private router: Router,
-    private _snackBar: MatSnackBar
-  ) {}
+    private _snackBar: MatSnackBar,
+    private fb: FormBuilder
+  ) {
+    this.brandForm = this.fb.group({
+      name: ["", [Validators.required]],
+      logo: [null, [Validators.required]],
+      categoryId: ["", [Validators.required]],
+      description: [null, []],
+    });
+  }
 
   ngOnInit(): void {}
 
@@ -25,22 +41,39 @@ export class BrandaddComponent implements OnInit {
   }
 
   cancelAdd() {
-    this.router.navigate(['/brands/list']);
+    this.router.navigate(["/brands/list"]);
   }
 
-  onClickSubmit(data) {
-    this.addBrands(data);
+  onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+
+      this.brandForm.patchValue({
+        logo: file,
+      });
+    }
   }
 
-  async addBrands(data) {
-    await (await this.BS.addBrands(data)).subscribe(
-      async (p) => {
-        this.openSnackBar("Brand Added...", "");
-        this.router.navigate(['/brands/list']);
-      },
-      (error) => {
-        console.log("error adding the brands");
-      }
-    );
+  async saveBrand() {
+    if (this.brandForm.valid) {
+      const formData = new FormData();
+      formData.append("name", this.brandForm.get("name").value);
+      formData.append("categoryId", this.brandForm.get("categoryId").value);
+      formData.append("description", this.brandForm.get("description").value);
+      formData.append("logo", this.brandForm.get("logo").value);
+
+      await (await this.BS.addBrands(formData)).subscribe(
+        async (p) => {
+          this.openSnackBar("Brand Added...", "");
+          this.router.navigate(["/brands/list"]);
+        },
+        (error) => {
+          console.log("error adding the brands");
+        }
+      );
+    } else {
+      this.openSnackBar("Please Enter Mandatory Fields!", "");
+      return false;
+    }
   }
 }
